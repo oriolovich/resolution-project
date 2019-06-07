19a94a70a8c7a24eab8cc269a2e8b42eb06a24e0
Oriol <ocg00@iesemilidarder.com>
2019-06-07 18:40:38 +0200

    ok

--- classes/artifacts/resolution_web_Web_exploded/src/main/java/com/iesemilidarder/finalproject/oriolovitx/resolution/web/Application.java
+++ classes/artifacts/resolution_web_Web_exploded/src/main/java/com/iesemilidarder/finalproject/oriolovitx/resolution/web/Application.java
@@ -2,18 +2,17 @@ package com.iesemilidarder.finalproject.oriolovitx.resolution.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
 
 @SpringBootApplication
-@EnableScheduling
-
-public class Application extends SpringBootServletInitializer {
 
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+
+public class Application{
+
+
+    public static
+    void main (String... args) {
+
     }
 }
